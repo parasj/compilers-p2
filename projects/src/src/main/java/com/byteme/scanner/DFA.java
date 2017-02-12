59d2b5fab8c2a4506aa5bc22fd07aa41b97463cb
Paras Jain <parasjain@parasjain.com>
2017-02-12 17:15:46 -0500

    DFA acceptance scaffold

--- projects/src/src/main/java/com/byteme/scanner/DFA.java
+++ projects/src/src/main/java/com/byteme/scanner/DFA.java
@@ -1,6 +1,7 @@
 package com.byteme.scanner;
 
 import java.util.*;
+import java.util.stream.IntStream;
 
 /**
  * src
@@ -11,11 +12,23 @@ public class DFA {
     private DFAState initial;
 
     public boolean accepts(String s) {
-        char[] chars = s.toCharArray();
-        return accepts(Arrays.asList(chars));
+        DFAState curr = initial;
+        return accepts(initial, s);
     }
 
-    public boolean accepts(List<Character> s) {
+    private boolean accepts(DFAState state, String str) {
+        // dead state -> FALSE
+        if (state == null)
+            return false;
 
+        // empty string -> state in accepting
+        if (str == null || str.length() == 0)
+            return accepting.contains(state);
+
+        // recurse on remainder of input
+        Map<Character, DFAState> delta = transitions.getOrDefault(str.charAt(0), Collections.emptyMap());
+        DFAState statePrime = delta.get(str.charAt(0));
+        String remaining = (str.length() > 0) ? str.substring(1) : "";
+        return accepts(statePrime, remaining);
     }
 }
