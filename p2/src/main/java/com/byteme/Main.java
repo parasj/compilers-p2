a19e614da61c3f7c02520b52b48fb222bdf42619
ebailey34 <evanbailey122@gatech.edu>
2017-03-13 12:20:41 -0400

    code restructuring and documentation in Main.java

--- p2/src/main/java/com/byteme/Main.java
+++ p2/src/main/java/com/byteme/Main.java
@@ -8,6 +8,7 @@ import com.byteme.lexer.Token;
 import com.byteme.lexer.classes.*;
 import com.byteme.scanner.Scanner;
 import org.dom4j.Document;
+import org.dom4j.DocumentException;
 
 import java.io.File;
 import java.util.ArrayList;
@@ -16,8 +17,6 @@ import java.util.LinkedList;
 
 public class Main {
 
-    // TODO: Move this to GrammarBuilder or Grammar or something
-    //Made public to use
     public static Lexeme[] lexemes = {
             new KeywordLexeme("array"),
             new KeywordLexeme("begin"),
@@ -75,6 +74,81 @@ public class Main {
             new IntlitClassLexeme()
     };
 
+    /*
+     * Called when using the --tokens command-line argument.
+     *
+     * For Project 1.
+     */
+    private static void doScanningPhase(String fileName) {
+        Scanner scanner = new Scanner(new File(fileName), lexemes);
+
+        // Generate Tokens for this file
+        scanner.tokenize();
+
+        // Iterate over generated Tokens
+        while (scanner.hasNextToken()) {
+            Token t = scanner.getNextToken();
+            Lexeme l = t.getLexeme();
+
+            // Format the token output
+            String tokenString = l.stringify(t.getValue());
+
+            // Only write tokenString if it is not empty
+            if (!tokenString.isEmpty()) {
+                System.out.print(l.stringify(t.getValue()));
+
+                if (scanner.hasNextToken())
+                    System.out.print(" ");
+            }
+        }
+    }
+
+    /*
+     * Called when using the --grammar command-line argument.
+     *
+     * For testing Grammar, GrammarBuilder, and grammar.xml during Project 2 development.
+     */
+    private static void doGrammarBuildingPhase() {
+        final String GRAMMAR_XML_PATH = "grammar.xml";
+
+        Lexeme[] grammarLexemes = new Lexeme[lexemes.length + 1];
+        Lexeme epsilonLexeme = new KeywordLexeme(""); // for matching epsilon (the empty sequence)
+        LinkedList<Lexeme> grammarLexemesList = new LinkedList<>();
+
+        // Build list of Lexemes for the GrammarBuilder
+        Collections.addAll(grammarLexemesList, lexemes);
+        grammarLexemesList.add(epsilonLexeme);
+        grammarLexemesList.toArray(grammarLexemes);
+
+        try {
+            Document doc = GrammarBuilder.parseXML(new File(GRAMMAR_XML_PATH));
+
+            System.out.println(GrammarBuilder.buildGrammar(doc, grammarLexemes));
+        }
+        catch (DocumentException dex) {
+            dex.printStackTrace();
+        }
+    }
+
+    /*
+     * Called when using the --ast command-line argument.
+     *
+     * For Project 2.
+     */
+    private static void doParsingPhase(String fileName) {
+        // TODO: Implement this once our parser is complete.
+        return;
+    }
+
+    /*
+     * Displays information about how to run this program.
+     */
+    private static void printUsage() {
+        System.out.println(
+                "Usage: java -jar compiler.jar SOURCE_FILE [--tokens | --grammar | --ast]\n"
+        );
+    }
+
     public static void main(String[] args) {
         if (args.length != 2) {
             printUsage();
@@ -84,53 +158,23 @@ public class Main {
         String fnameIn = args[0];
         String phase = args[1];
 
-        if (phase.equals("--tokens")) {
-            Scanner sc = new Scanner(new File(fnameIn), lexemes);
+        switch (phase) {
+            case "--tokens":
+                doScanningPhase(fnameIn);
+                break;
 
-            // Generate tokens
-            sc.tokenize();
+            case "--grammar":
+                doGrammarBuildingPhase();
+                break;
 
-            while (sc.hasNextToken()) {
-                Token t = sc.getNextToken();
-                Lexeme l = t.getLexeme();
+            case "--ast":
+                doParsingPhase(fnameIn);
+                break;
 
-                // Format the token output
-                String tokenString = l.stringify(t.getValue());
-
-                // Only write tokenString if it is not empty
-                if (!tokenString.isEmpty()) {
-                    System.out.print(l.stringify(t.getValue()));
-
-                    if (sc.hasNextToken())
-                        System.out.print(" ");
-                }
-            }
+            default:
+                printUsage();
+                System.exit(1);
+                break;
         }
-        else if (phase.equals("--grammar")) {
-            final String GRAMMAR_XML_PATH = "grammar.xml";
-
-            try {
-                Lexeme[] grammarLexemes = new Lexeme[lexemes.length + 1];
-                LinkedList<Lexeme> grammarLexemesList = new LinkedList<>();
-                Document doc = GrammarBuilder.parseXML(new File(GRAMMAR_XML_PATH));
-
-                Collections.addAll(grammarLexemesList, lexemes);
-                grammarLexemesList.add(new KeywordLexeme("")); // to match epsilon
-                grammarLexemesList.toArray(grammarLexemes);
-
-                System.out.println(GrammarBuilder.buildGrammar(doc, grammarLexemes));
-            }
-            catch (Exception ex) {
-                ex.printStackTrace();
-            }
-        }
-        else {
-            printUsage();
-            System.exit(1);
-        }
-    }
-
-    private static void printUsage() {
-        System.out.println("Usage: java -jar compiler.jar SOURCE_FILE [—-tokens]\n");
     }
 }
